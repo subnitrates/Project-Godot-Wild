17eafda06ebc9dab14c8bc37fbbc5249bc2702ca
Subnitrates <ch2435@gmail.com>
2025-06-06 14:30:47 -0500

    hover gun

---
+++ Player/Guns/hover_gun.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var radius: float = 10.0
+@export var lerp_speed: float = 5.0
+
+var _target_position: Vector2 = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	global_position = global_position.lerp(_target_position, lerp_speed * delta)
+	look_at(get_global_mouse_position())
+
+func update_position(player_position: Vector2, mouse_position: Vector2) -> void:
+	var direction = (mouse_position - player_position).normalized()
+	_target_position = player_position + (direction * radius)
--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,7 @@ var current_state: PlayerState
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var hover_gun = $HoverGun
 
 func _ready() -> void:
 	current_state = PlayerIdleState.new()
@@ -14,7 +15,8 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if current_state:
 		current_state.update(delta)
-	
+	hover_gun.update_position(global_position, get_global_mouse_position())
+
 func change_state(new_state_instance: PlayerState):
 	if current_state:
 		current_state.exit()
