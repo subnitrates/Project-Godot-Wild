43769d89677831dbe87f72ddb9898319a6b51312
Subnitrates <ch2435@gmail.com>
2025-06-06 14:38:42 -0500

    bullets

--- Player/Guns/hover_gun.gd
+++ Player/Guns/hover_gun.gd
@@ -5,6 +5,12 @@ extends Node2D
 
 var _target_position: Vector2 = Vector2.ZERO
 
+func _ready():
+	# pulsing effect
+	var tween = create_tween().set_loops()
+	tween.tween_property(self, "scale", Vector2(1.1, 1.1), 0.5)
+	tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.5)
+
 func _physics_process(delta: float) -> void:
 	global_position = global_position.lerp(_target_position, lerp_speed * delta)
 	look_at(get_global_mouse_position())
