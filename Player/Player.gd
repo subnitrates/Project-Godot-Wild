89de7e5a93e3d39300c54e5b5590e4a050882ff4
Subnitrates <ch2435@gmail.com>
2025-06-06 14:50:22 -0500

    bullets and shooting

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,6 +7,9 @@ var current_state: PlayerState
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hover_gun = $HoverGun
 
+var is_shooting: bool = false
+
+
 func _ready() -> void:
 	current_state = PlayerIdleState.new()
 	current_state.player = self
@@ -16,6 +19,15 @@ func _physics_process(delta: float) -> void:
 	if current_state:
 		current_state.update(delta)
 	hover_gun.update_position(global_position, get_global_mouse_position())
+	
+	if Input.is_action_pressed("shoot"):
+		hover_gun.shoot()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("shoot"):
+		is_shooting = true
+	elif event.is_action_released("shoot"):
+		is_shooting = false
 
 func change_state(new_state_instance: PlayerState):
 	if current_state:
